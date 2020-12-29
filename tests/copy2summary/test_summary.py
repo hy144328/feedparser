39e73e646dad912d85eee345f1dfbb9b74fc13c6
Hans Yu <hans.yu@outlook.de>
2020-12-29 20:43:25 +0100

    Complete test case coverage.

--- tests/copy2summary/test_summary.py
+++ tests/copy2summary/test_summary.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 import feedparser
-import itertools
+from itertools import chain, permutations
 import pytest
 
 test_summary_args = ["content", "description", "summary"]
 
-@pytest.mark.parametrize("a,b,c", list(itertools.permutations(test_summary_args)))
-def test_summary(a, b, c):
-    with open(f"tests/copy2summary/item_{a}_{b}_{c}.xml", 'r') as f:
+def powerset(iterable):
+    "powerset([1,2,3]) --> (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(permutations(s, r) for r in range(1, len(s)+1))
+
+@pytest.mark.parametrize("a", list(powerset(test_summary_args)))
+def test_summary(a):
+    filename = "_".join(["tests/copy2summary/item"] + list(a)) + ".xml"
+    with open(filename, 'r') as f:
         d = feedparser.parse(f.read())
-    assert(d.entries[0].summary == "Example summary")
+
+    if "summary" in a:
+        assert(d.entries[0].summary == "Example summary")
+    elif "description" in a:
+        assert(d.entries[0].summary == "Example description")
+    elif "content" in a:
+        assert(d.entries[0].summary == "Example content")
